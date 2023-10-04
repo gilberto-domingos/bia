b204b2f3195707141a76734882339ba9f703b3a4
jrdomingos <domingoshot@hotmail.com>
2023-10-04 18:57:54 -0300

    project AWS test my scripts CloudShel

---
+++ README.md
@@ -0,0 +1,6 @@
+##  AWS my scripts test CloudShel
+
+#### For run migrations on container ####
+```
+docker-compose exec server bash -c 'npx sequelize db:migrate'
+```
---
+++ client/src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+const About = () => {
+  return (
+    <div>
+      <h4>Versão 2.0.0</h4>
+      <h5>BIA 02 a 08 de Outubro/2023</h5>
+      <Link to="/">Voltar</Link>
+    </div>
+  );
+};
+
+export default About;
